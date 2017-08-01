792279713075ef334185c83441080afeca777e1c
Diego Caminos <d.caminos54@gmail.com>
2017-08-01 00:14:24 -0300

    added TileMap

---
+++ src/main/java/scene/game/RichGameComponent.java
@@ -0,0 +1,9 @@
+package scene.game;
+
+import com.uqbar.vainilla.GameComponent;
+
+/**
+ * Created by Pelotita on 31/7/2017.
+ */
+public class RichGameComponent extends GameComponent<Battlefield> {
+}
---
+++ src/main/java/scene/game/TileMap.java
@@ -0,0 +1,14 @@
+package scene.game;
+
+import com.uqbar.vainilla.GameComponent;
+
+/**
+ * Created by Pelotita on 29/7/2017.
+ */
+public class TileMap {
+    GameComponent[][] map;
+
+    public TileMap(){
+        map = new GameComponent[15][13];
+    }
+}
